c781c9bae0e24d029f1e47c25c4544524fbd76dd
Eric-Mai-25 <ericmai2@gmail.com>
2025-01-11 15:24:46 -0800

    decode

---
+++ DecodeWAys.py
@@ -0,0 +1,18 @@
+def num_decodings(s: str) -> int:
+    if not s or s[0] == "0":
+        return 0
+    
+    dp = [0] * (len(s) + 1)
+    dp[0], dp[1] = 1, 1
+
+    for i in range(2, len(s) + 1):
+        if 1 <= int(s[i-1:i]) <= 9:
+            dp[i] += dp[i-1]
+        if 10 <= int(s[i-2:i]) <= 26:
+            dp[i] += dp[i-2]
+    
+    return dp[-1]
+
+# Example
+print(num_decodings("12"))   # 2 ("AB" or "L")
+print(num_decodings("226"))  # 3 ("BZ", "VF", "BBF")
